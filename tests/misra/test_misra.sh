2ef967b78ad11fd13e7ae2debaf559e7588d83e7
ReFil <31960031+ReFil@users.noreply.github.com>
2021-05-05 13:41:46 +0100

    Update test_misra.sh

--- tests/misra/test_misra.sh
+++ tests/misra/test_misra.sh
@@ -32,6 +32,42 @@ python /usr/share/cppcheck/addons/misra.py $PANDA_DIR/board/pedal/main.c.dump 2>
 cppcheck_pedal_output=$( cat /tmp/misra/cppcheck_pedal_output.txt | grep -v ": information: " ) || true
 misra_pedal_output=$( cat /tmp/misra/misra_pedal_output.txt | grep -v ": information: " ) || true
 
+printf "\nBETTER PEDAL CODE\n"
+cppcheck -UPANDA -DPEDAL -UCAN3 \
+         --suppressions-list=suppressions.txt \
+         -I $PANDA_DIR/board/ --dump --enable=all --inline-suppr --force \
+         $PANDA_DIR/board/better_pedal/main.c 2>/tmp/misra/cppcheck_better_pedal_output.txt
+
+python /usr/share/cppcheck/addons/misra.py $PANDA_DIR/board/better_pedal/main.c.dump 2> /tmp/misra/misra_better_pedal_output.txt || true
+
+# strip (information) lines
+cppcheck_better_pedal_output=$( cat /tmp/misra/cppcheck_better_pedal_output.txt | grep -v ": information: " ) || true
+misra_better_pedal_output=$( cat /tmp/misra/misra_better_pedal_output.txt | grep -v ": information: " ) || true
+
+printf "\nIBST CODE\n"
+cppcheck -UPANDA -DIBST -UCAN3 \
+         --suppressions-list=suppressions.txt \
+         -I $PANDA_DIR/board/ --dump --enable=all --inline-suppr --force \
+         $PANDA_DIR/board/ibst/main.c 2>/tmp/misra/cppcheck_ibst_output.txt
+
+python /usr/share/cppcheck/addons/misra.py $PANDA_DIR/board/ibst/main.c.dump 2> /tmp/misra/misra_ibst_output.txt || true
+
+# strip (information) lines
+cppcheck_ibst_output=$( cat /tmp/misra/cppcheck_isbt_output.txt | grep -v ": information: " ) || true
+misra_ibst_output=$( cat /tmp/misra/misra_ibst_output.txt | grep -v ": information: " ) || true
+
+printf "\nCTRLS CODE\n"
+cppcheck -UPANDA -DCTRLS -UCAN3 \
+         --suppressions-list=suppressions.txt \
+         -I $PANDA_DIR/board/ --dump --enable=all --inline-suppr --force \
+         $PANDA_DIR/board/ctrls/main.c 2>/tmp/misra/cppcheck_ctrls_output.txt
+
+python /usr/share/cppcheck/addons/misra.py $PANDA_DIR/board/ctrls/main.c.dump 2> /tmp/misra/misra_ctrls_output.txt || true
+
+# strip (information) lines
+cppcheck_ctrls_output=$( cat /tmp/misra/cppcheck_ctrls_output.txt | grep -v ": information: " ) || true
+misra_ctrls_output=$( cat /tmp/misra/misra_ctrls_output.txt | grep -v ": information: " ) || true
+
 if [[ -n "$misra_output" ]] || [[ -n "$cppcheck_output" ]]
 then
   echo "Failed! found Misra violations in panda code:"
@@ -48,4 +84,27 @@ then
   exit 1
 fi
 
+if [[ -n "$misra_better_pedal_output" ]] || [[ -n "$cppcheck_better_pedal_output" ]]
+then
+  echo "Failed! found Misra violations in better_pedal code:"
+  echo "$misra_better_pedal_output"
+  echo "$cppcheck_better_pedal_output"
+  exit 1
+fi
+
+if [[ -n "$misra_ibst_output" ]] || [[ -n "$cppcheck_ibst_output" ]]
+then
+  echo "Failed! found Misra violations in ibst code:"
+  echo "$misra_ibst_output"
+  echo "$cppcheck_ibst_output"
+  exit 1
+fi
+
+if [[ -n "$misra_ctrls_output" ]] || [[ -n "$cppcheck_ctrls_output" ]]
+then
+  echo "Failed! found Misra violations in ctrls code:"
+  echo "$misra_ctrls_output"
+  echo "$cppcheck_ctrls_output"
+  exit 1
+fi
 echo "Success"
